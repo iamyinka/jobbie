4f8cf6fca627865063a3b3b77a9f24c5d02dba24
Yinka Ash <developer@designsbynetguru.com>
2018-02-20 14:15:49 +0800

    Finish Portal

---
+++ blog-full.php
@@ -0,0 +1,272 @@
+<?php include('includes/styles.php'); ?>
+
+
+
+<body class="theme-style-1">
+
+<!--WRAPPER START-->
+
+<div id="wrapper"> 
+
+  <!--HEADER START-->
+
+  <?php include('includes/header.php'); ?>
+  <!--HEADER END--> 
+
+  
+
+  <!--INNER BANNER START-->
+
+  <section id="inner-banner">
+
+    <div class="container">
+
+      <h1>Blog Large</h1>
+
+    </div>
+
+  </section>
+
+  <!--INNER BANNER END--> 
+
+  
+
+  <!--MAIN START-->
+
+  <div id="main"> 
+
+    
+
+    <!--POST AREA START-->
+
+    <section class="post-section blog-post">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-9 col-sm-8">
+
+            <div class="post-box">
+
+              <div class="frame"><a href="#"><img src="images/blog/blog-full-img-1.jpg" alt="img"></a></div>
+
+              <div class="text-box"> <strong class="date"><i class="fa fa-calendar"></i>Mar 17, 2016 </strong>
+
+                <h4><a href="#">Middle Class jobs are being replaced by burger-flipping, retail sales.</a></h4>
+
+                <p>Donec volutpat enim at interdum pretium. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec sodales mauris pretium scelerisque lobortis. uspendisse id pharetra lacus, et hendrerit mi. Praesent at vestibulum tortor. Praesent condimentum efficiturus sed. Vestibulum ac tincidunt enim.</p>
+
+                <div class="thumb"><a href="#"><img src="images/admin-img.jpg" alt="img"></a></div>
+
+                <strong class="name"><span>By</span> Anna Smith</strong> <a href="#" class="btn-style-1">Read More</a> </div>
+
+            </div>
+
+            <div class="post-box">
+
+              <div id="blog-slider-post" class="owl-carousel owl-theme">
+
+                <div class="item">
+
+                  <div class="frame"><a href="#"><img src="images/blog/blog-full-img-2.jpg" alt="img"></a></div>
+
+                </div>
+
+                <div class="item">
+
+                  <div class="frame"><a href="#"><img src="images/blog/blog-full-img-1.jpg" alt="img"></a></div>
+
+                </div>
+
+              </div>
+
+              <div class="text-box"> <strong class="date"><i class="fa fa-calendar"></i>Mar 17, 2016 </strong>
+
+                <h4><a href="#">Middle Class jobs are being replaced by burger-flipping, retail sales.</a></h4>
+
+                <p>Donec volutpat enim at interdum pretium. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec sodales mauris pretium scelerisque lobortis. uspendisse id pharetra lacus, et hendrerit mi. Praesent at vestibulum tortor. Praesent condimentum efficiturus sed. Vestibulum ac tincidunt enim.</p>
+
+                <div class="thumb"><a href="#"><img src="images/admin-img.jpg" alt="img"></a></div>
+
+                <strong class="name"><span>By</span> Anna Smith</strong> <a href="#" class="btn-style-1">Read More</a> </div>
+
+            </div>
+
+            <div class="post-box">
+
+              <div class="thumb">
+
+                <iframe src="https://player.vimeo.com/video/83884718" style="width:100%; height:472px; border:0;"></iframe>
+
+              </div>
+
+              <div class="text-box"> <strong class="date"><i class="fa fa-calendar"></i>Mar 17, 2016 </strong>
+
+                <h4><a href="#">Middle Class jobs are being replaced by burger-flipping, retail sales.</a></h4>
+
+                <p>Donec volutpat enim at interdum pretium. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec sodales mauris pretium scelerisque lobortis. uspendisse id pharetra lacus, et hendrerit mi. Praesent at vestibulum tortor. Praesent condimentum efficiturus sed. Vestibulum ac tincidunt enim.</p>
+
+                <div class="thumb"><a href="#"><img src="images/admin-img.jpg" alt="img"></a></div>
+
+                <strong class="name"><span>By</span> Anna Smith</strong> <a href="#" class="btn-style-1">Read More</a> </div>
+
+            </div>
+
+            <div class="post-box">
+
+              <div class="frame"><a href="#"><img src="images/blog/blog-full-img-3.jpg" alt="img"></a></div>
+
+              <div class="text-box"> <strong class="date"><i class="fa fa-calendar"></i>Mar 17, 2016 </strong>
+
+                <h4><a href="#">Middle Class jobs are being replaced by burger-flipping, retail sales.</a></h4>
+
+                <p>Donec volutpat enim at interdum pretium. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec sodales mauris pretium scelerisque lobortis. uspendisse id pharetra lacus, et hendrerit mi. Praesent at vestibulum tortor. Praesent condimentum efficiturus sed. Vestibulum ac tincidunt enim.</p>
+
+                <div class="thumb"><a href="#"><img src="images/admin-img.jpg" alt="img"></a></div>
+
+                <strong class="name"><span>By</span> Anna Smith</strong> <a href="#" class="btn-style-1">Read More</a> </div>
+
+            </div>
+
+            <!--PAGINATION SECTION START-->
+
+            <div class="pagination-box">
+
+              <nav>
+
+                <ul class="pager">
+
+                  <li><a href="#" class="prv">Previous</a></li>
+
+                  <li><a href="#">1</a></li>
+
+                  <li class="active"><a href="#">2</a></li>
+
+                  <li><a href="#">3</a></li>
+
+                  <li><a href="#">4</a></li>
+
+                  <li><a href="#" class="next">Next</a></li>
+
+                </ul>
+
+              </nav>
+
+            </div>
+
+            <!--PAGINATION SECTION END--> 
+
+          </div>
+
+          <div class="col-md-3 col-sm-4">
+
+            <aside>
+
+              <div class="sidebar">
+
+                <h4>Text Widget</h4>
+
+                <div class="box">
+
+                  <div class="frame"><a href="#"><img src="images/blog/blog-test-widget.jpg" alt="img"></a></div>
+
+                  <div class="text-col">
+
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusa doloremque laudantium, totam rem aperiam eaque ipsa quae.</p>
+
+                  </div>
+
+                </div>
+
+                <h4>Recent Posts</h4>
+
+                <div class="box">
+
+                  <div class="recent-post">
+
+                    <ul>
+
+                      <li> <strong><a href="#">Middle Class jobs are being
+
+                        replaced by flipping</a></strong>
+
+                        <div class="img-frame"><a href="#"><img src="images/blog/recent-img-1.jpg" alt="img"></a></div>
+
+                        <div class="text-area"> <a href="#"><i class="fa fa-user"></i>Dana Lincon</a> <a href="#"><i class="fa fa-calendar"></i>Mar 17, 2016 </a> </div>
+
+                      </li>
+
+                      <li> <strong><a href="#">Middle Class jobs are being
+
+                        replaced by flipping</a></strong>
+
+                        <div class="img-frame"><a href="#"><img src="images/blog/recent-img-2.jpg" alt="img"></a></div>
+
+                        <div class="text-area"> <a href="#"><i class="fa fa-user"></i>Austin Cherrist</a> <a href="#"><i class="fa fa-calendar"></i>Feb 20, 2016</a> </div>
+
+                      </li>
+
+                      <li> <strong><a href="#">Middle Class jobs are being
+
+                        replaced by flipping</a></strong>
+
+                        <div class="img-frame"><a href="#"><img src="images/blog/recent-img-3.jpg" alt="img"></a></div>
+
+                        <div class="text-area"> <a href="#"><i class="fa fa-user"></i>John Doe</a> <a href="#"><i class="fa fa-calendar"></i>Feb 09, 2016 </a> </div>
+
+                      </li>
+
+                    </ul>
+
+                  </div>
+
+                </div>
+
+                <h4>Archives</h4>
+
+                <div class="box">
+
+                  <div class="archives-box">
+
+                    <ul>
+
+                      <li><a href="#">January 2016<span>(10)</span></a></li>
+
+                      <li><a href="#">August 2015<span>(14)</span></a></li>
+
+                      <li><a href="#">February 2015<span>(20)</span></a></li>
+
+                      <li><a href="#">December 2014<span>(07)</span></a></li>
+
+                      <li><a href="#">July 2014<span>(05)</span></a></li>
+
+                    </ul>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </aside>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+    <!--POST AREA END--> 
+
+  </div>
+
+  <!--MAIN END--> 
+
+  
+
+  <!--FOOTER START-->
+
+  <?php include('includes/footer.php'); ?>
